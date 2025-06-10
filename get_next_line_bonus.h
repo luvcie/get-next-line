b10cca2351b883ada0b82d1d8a06d3c815640afa
luvcie <lucpardo@student.42.fr>
2025-06-10 21:52:09 +0200

    change return type of ft_free_and_zero

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: lucpardo <lucpardo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 00:51:33 by lucpardo          #+#    #+#             */
-/*   Updated: 2025/06/04 00:51:46 by lucpardo         ###   ########.fr       */
+/*   Updated: 2025/06/10 21:51:47 by lucpardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,6 @@ size_t	ft_strlen(const char *str);
 char	*ft_strchr(const char *str, int c);
 char	*ft_gnl_substr(char const *str, unsigned int start, size_t len);
 char	*ft_gnl_strjoin(char *s1, char const *s2);
-void	*ft_free_and_zero(char **ptr);
+void	ft_free_and_zero(char **ptr);
 
 #endif
